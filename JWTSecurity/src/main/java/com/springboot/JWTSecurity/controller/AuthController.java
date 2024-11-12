952a934d586637adfa6bcc6f4ee39a699f466201
AnujDev667 <142241069+AnujDev667@users.noreply.github.com>
2024-11-12 12:42:29 +0530

    JWTBasics

---
+++ JWTSecurity/src/main/java/com/springboot/JWTSecurity/controller/AuthController.java
@@ -0,0 +1,10 @@
+package com.springboot.JWTSecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class AuthController {
+	@GetMapping("/api/token")
+	public void getToken() {
+		
+	}
+}
